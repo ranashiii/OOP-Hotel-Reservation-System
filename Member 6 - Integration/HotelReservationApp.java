45dd751f3b363da2e968b7d390c671a0ec66eccd
RS Arciaga <ranaarciaga@gmail.com>
2026-06-11 10:14:08 +0800

    Remove member folders, keep only Hotelv10-main

--- Member 6 - Integration/HotelReservationApp.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.guestinterface;
-public class HotelReservationApp {
-
-    public static void main(String[] args) {     
-    SessionManager.init();
-      
-    new LoginFrame().setVisible(true);
-    }
-}
